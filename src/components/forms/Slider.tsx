ad781af9bbf80027150efd32bb4951f9e8c15dc4
whey.party <did:plc:mn45tewwnse5btfftvd3powc>
2025-12-02 02:05:59 +0100

    Revert "attempt 3 of slider android crash fix"
    This reverts commit d6da5ec34a0a01074a2de727706e90e13dce0f7f.

--- src/components/forms/Slider.tsx
+++ src/components/forms/Slider.tsx
@@ -1,7 +1,5 @@
-import {useCallback, useEffect, useMemo, useState} from 'react'
 import {type ViewStyle} from 'react-native'
-import RNSlider from '@react-native-community/slider'
-import {throttle} from 'lodash'
+import {Slider as RNSlider} from '@miblanchard/react-native-slider'
 
 import {useTheme} from '#/alf'
 
@@ -11,7 +9,10 @@ interface SliderProps {
   minimumValue?: number
   maximumValue?: number
   step?: number
-  sliderStyle?: ViewStyle
+  trackStyle?: ViewStyle
+  minimumTrackStyle?: ViewStyle
+  thumbStyle?: ViewStyle
+  thumbTouchSize?: {width: number; height: number}
 }
 
 export function Slider({
@@ -20,62 +21,47 @@ export function Slider({
   minimumValue = 0,
   maximumValue = 1,
   step = 1,
-  sliderStyle,
+  trackStyle,
+  minimumTrackStyle,
+  thumbStyle,
+  thumbTouchSize = {width: 40, height: 40},
 }: SliderProps) {
   const t = useTheme()
 
-  const [localValue, setLocalValue] = useState(value)
-
-  useEffect(() => {
-    setLocalValue(value)
-  }, [value])
-
-  const throttledOnValueChange = useMemo(
-    () =>
-      throttle(
-        (val: number) => {
-          onValueChange(val)
-        },
-        50,
-        {leading: true, trailing: true},
-      ),
-    [onValueChange],
-  )
-
-  const handleValueChange = useCallback(
-    (val: number) => {
-      setLocalValue(val)
-      throttledOnValueChange(val)
-    },
-    [throttledOnValueChange],
-  )
-
-  const handleSlidingComplete = useCallback(
-    (val: number) => {
-      throttledOnValueChange.cancel()
-      onValueChange(val)
-    },
-    [throttledOnValueChange, onValueChange],
-  )
-
   return (
     <RNSlider
-      value={localValue}
-      onValueChange={handleValueChange}
-      onSlidingComplete={handleSlidingComplete}
+      value={[value]} // always an array
+      onValueChange={values => onValueChange(values[0])}
       minimumValue={minimumValue}
       maximumValue={maximumValue}
       step={step}
-      minimumTrackTintColor={t.palette.primary_500}
-      maximumTrackTintColor={t.atoms.bg_contrast_50.backgroundColor}
-      thumbTintColor={t.atoms.bg_contrast_500.backgroundColor}
-      thumbImage={undefined}
-      style={[
-        {
-          height: 40,
-        },
-        sliderStyle,
-      ]}
+      trackStyle={{
+        height: 4,
+        borderRadius: 2,
+        backgroundColor: t.atoms.bg_contrast_50.backgroundColor,
+        ...trackStyle,
+      }}
+      minimumTrackStyle={{
+        height: 4,
+        borderRadius: 2,
+        backgroundColor: t.palette.primary_500,
+        ...minimumTrackStyle,
+      }}
+      thumbStyle={{
+        width: 24,
+        height: 24,
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: t.atoms.border_contrast_low.borderColor,
+        backgroundColor: t.atoms.bg.backgroundColor,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.15,
+        shadowRadius: 4,
+        elevation: 3,
+        ...thumbStyle,
+      }}
+      thumbTouchSize={thumbTouchSize}
     />
   )
 }
